2788efdedd8fdcf87103388c4a40f5df6f075e0c
Sarah Moore <sfmoore99@gmail.com>
2015-08-31 12:36:55 -0700

    Completed through interfaces

---
+++ about_common_interfaces.go
@@ -0,0 +1,34 @@
+package go_koans
+
+import "bytes"
+
+func aboutCommonInterfaces() {
+	{
+		in := new(bytes.Buffer)
+		in.WriteString("hello world")
+
+		out := new(bytes.Buffer)
+
+		/*
+		   Your code goes here.
+		   Hint, use these resources:
+
+		   $ godoc -http=:8080
+		   $ open http://localhost:8080/pkg/io/
+		   $ open http://localhost:8080/pkg/bytes/
+		*/
+		 in.WriteTo(out)  // or out.ReadFrom(in)
+
+		assert(out.String() == "hello world") // get data from the io.Reader to the io.Writer
+	}
+
+	{
+		in := new(bytes.Buffer)
+		in.WriteString("hello world")
+
+		out := new(bytes.Buffer)
+		out.Write(in.Next(5))
+
+		assert(out.String() == "hello") // duplicate only a portion of the io.Reader
+	}
+}
